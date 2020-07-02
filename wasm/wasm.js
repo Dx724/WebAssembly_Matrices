070c4c0163d15bfd3428af3c5b7918cf096dd689
Dx724 <dx72dx@gmail.com>
2020-07-02 19:50:44 -0400

    Added WebAssembly implementation

---
+++ wasm/wasm.js
@@ -0,0 +1,19 @@
+fetch("wasm.wasm").then(wasm => {
+    //startTime = performance.now();
+
+    var importObject = {
+        console: {
+            log: (value) => console.log(value)
+        },
+        js: {
+            mem: new WebAssembly.Memory({initial: 1}),
+            global: new WebAssembly.Global({value: "i32", mutable: true}, 0)
+        }
+    };
+
+    WebAssembly.instantiate(wasm, importObject).then(obj => {
+        obj.instance.exports.runCode();
+    });
+
+    //console.log("Total Time:" + (performance.now() - startTime));
+});

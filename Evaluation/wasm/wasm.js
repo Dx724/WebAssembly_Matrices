b49ba44ccc87fc526aad583a69bed5312cd9582a
Dx724 <dx72dx@gmail.com>
2020-08-03 19:38:01 -0400

    Added performance test
    100x100 matrix multiplication done 10000 times

---
+++ Evaluation/wasm/wasm.js
@@ -0,0 +1,18 @@
+fetch("wasm.wasm").then(wasm => {wasm.arrayBuffer().then((wasm) => {
+    startTime = performance.now();
+
+    importObject = {
+        console: {
+            log: (value) => console.log(value)
+        },
+        js: {
+            mem: new WebAssembly.Memory({initial: 32767}),
+            global: new WebAssembly.Global({value: "i32", mutable: true}, 0)
+        }
+    };
+
+    WebAssembly.instantiate(wasm, importObject).then(obj => {
+        obj.instance.exports.runCode();
+        console.log("Total Time: " + (performance.now() - startTime) + " milliseconds");
+    });
+})});
